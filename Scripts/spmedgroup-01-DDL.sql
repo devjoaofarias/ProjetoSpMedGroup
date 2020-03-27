fb2220153d31f7ebcfd089c69a81c2ff4b3604b0
Joao Santos <joao.santos.farias.2003@gmail.com>
2020-03-27 02:02:23 -0300

    Banco de dados dentro da normalização

--- Scripts/spmedgroup-01-DDL.sql
+++ Scripts/spmedgroup-01-DDL.sql
@@ -1,73 +1,92 @@
-CREATE DATABASE SpMedGroup
+CREATE DATABASE SpMedGroup_Tarde;
+GO
 
-USE SpMedGroup
+USE SpMedGroup_Tarde;
+GO
+
+CREATE TABLE Estado (
+	IdEstado				INT PRIMARY KEY IDENTITY,
+	NomeEstado				VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE Cidade (
+	IdCidade				INT PRIMARY KEY IDENTITY,
+	IdEstado				INT FOREIGN KEY REFERENCES Estado(IdEstado),
+	NomeCidade				VARCHAR(100) NOT NULL
+);
 
 CREATE TABLE Clinica (
-IdClinica				INT PRIMARY KEY IDENTITY,
-Endereco				VARCHAR(150),
-Cnpj					CHAR(14) NOT NULL,
-HorarioFuncionamento	VARCHAR(15)
+	IdClinica				INT PRIMARY KEY IDENTITY,
+	IdCidade				INT FOREIGN KEY REFERENCES Cidade(IdCidade) NOT NULL,
+	NomeClinica				VARCHAR(100) NOT NULL,
+	Cnpj					CHAR(14) UNIQUE NOT NULL,
+	Cep						CHAR(8) UNIQUE NOT NULL,
+	Logradouro				VARCHAR(100) NOT NULL,
+	Bairro					VARCHAR(75) NOT NULL,
+	HorarioInicial			VARCHAR(15) NOT NULL,
+	HorarioFinal			VARCHAR(15) NOT NULL,
 );
 
 CREATE TABLE TipoUsuario (
-IdTipoUsuario			INT PRIMARY KEY IDENTITY,
-Titulo					VARCHAR(20) NOT NULL UNIQUE
+	IdTipoUsuario			INT PRIMARY KEY IDENTITY,
+	Titulo					VARCHAR(20) NOT NULL UNIQUE
 );
 
 CREATE TABLE Especialidade (
-IdEspecialidade			INT PRIMARY KEY IDENTITY,
-Titulo					VARCHAR(50) UNIQUE
+	IdEspecialidade			INT PRIMARY KEY IDENTITY,
+	Titulo					VARCHAR(50) UNIQUE
 );
 
 CREATE TABLE Usuario (
-IdUsuario				INT PRIMARY KEY IDENTITY,
-IdTipoUsuario			INT FOREIGN KEY REFERENCES TipoUsuario(IdTipoUsuario),
-Nome					VARCHAR(100) NOT NULL,
-Email					VARCHAR(100) NOT NULL UNIQUE,
-Senha					VARCHAR(20) NOT NULL,
-Telefone				VARCHAR(30) NOT NULL,
-Rg						CHAR(9) UNIQUE,
-Cpf						CHAR(11) UNIQUE,
-Endereco				VARCHAR(150)
+	IdUsuario				INT PRIMARY KEY IDENTITY,
+	IdTipoUsuario			INT FOREIGN KEY REFERENCES TipoUsuario(IdTipoUsuario),
+	Nome					VARCHAR(100) NOT NULL,
+	Email					VARCHAR(100) NOT NULL UNIQUE,
+	Senha					VARCHAR(20) NOT NULL,
+	Telefone				VARCHAR(30) NOT NULL,
+	Rg						CHAR(9) UNIQUE ,
+	Cpf						CHAR(11) UNIQUE NOT NULL,
+	Cep						CHAR(8),
+	Logradouro				VARCHAR(100),
+	Bairro					VARCHAR(50)
 );
 
 CREATE TABLE Administrador (
-IdAdministrador			INT PRIMARY KEY IDENTITY,
-IdClinica				INT FOREIGN KEY REFERENCES Clinica(IdClinica),
-IdUsuario				INT FOREIGN KEY REFERENCES Usuario(IdUsuario)
+	IdAdministrador			INT PRIMARY KEY IDENTITY,
+	IdClinica				INT FOREIGN KEY REFERENCES Clinica(IdClinica),
+	IdUsuario				INT FOREIGN KEY REFERENCES Usuario(IdUsuario)
 );
 
 CREATE TABLE Medico (
-IdMedico				INT PRIMARY KEY IDENTITY,
-IdEspecialidade			INT FOREIGN KEY REFERENCES Especialidade(IdEspecialidade),
-IdClinica				INT FOREIGN KEY REFERENCES Clinica(IdClinica),
-IdUsuario				INT FOREIGN KEY REFERENCES Usuario(IdUsuario),
-Crm						VARCHAR(15) NOT NULL UNIQUE
+	IdMedico				INT PRIMARY KEY IDENTITY,
+	IdEspecialidade			INT FOREIGN KEY REFERENCES Especialidade(IdEspecialidade),
+	IdClinica				INT FOREIGN KEY REFERENCES Clinica(IdClinica),
+	IdUsuario				INT FOREIGN KEY REFERENCES Usuario(IdUsuario),
+	Crm						VARCHAR(15) NOT NULL UNIQUE
 );
 
 CREATE TABLE Prontuario (
-IdProntuario			INT PRIMARY KEY IDENTITY,
-IdUsuario				INT FOREIGN KEY REFERENCES Usuario(IdUsuario),
-IdClinica				INT FOREIGN KEY REFERENCES Clinica(IdClinica),
-Altura					VARCHAR(15),
-DataNascimento			DATE NOT NULL,
-Peso					VARCHAR(8)
+	IdProntuario			INT PRIMARY KEY IDENTITY,
+	IdUsuario				INT FOREIGN KEY REFERENCES Usuario(IdUsuario),
+	IdClinica				INT FOREIGN KEY REFERENCES Clinica(IdClinica),
+	Altura					VARCHAR(15),
+	DataNascimento			DATE NOT NULL,
+	Peso					VARCHAR(7)
+);
+
+CREATE TABLE Situacao (
+	IdSituacao				INT PRIMARY KEY IDENTITY,
+	NomeSituacao			VARCHAR(35)
 );
 
 CREATE TABLE Consulta (
-IdConsulta				INT PRIMARY KEY IDENTITY,
-IdProntuario			INT FOREIGN KEY REFERENCES Prontuario(IdProntuario),
-IdMedico				INT FOREIGN KEY REFERENCES Medico(IdMedico),
-DataConsulta			DATE NOT NULL,
-Descricao				VARCHAR(100),
-Situacao				VARCHAR(50) NOT NULL
+	IdConsulta				INT PRIMARY KEY IDENTITY,
+	IdProntuario			INT FOREIGN KEY REFERENCES Prontuario(IdProntuario) NOT NULL,
+	IdMedico				INT FOREIGN KEY REFERENCES Medico(IdMedico) NOT NULL,
+	IdSituacao				INT FOREIGN KEY REFERENCES Situacao(IdSituacao) NOT NULL,
+	DataConsulta			DATE NOT NULL,
+	Descricao				VARCHAR(100),
 );
 
-SELECT * FROM Clinica
-SELECT * FROM Administrador
-SELECT * FROM Usuario
-SELECT * FROM Medico
-SELECT * FROM Prontuario
-SELECT * FROM Consulta
-SELECT * FROM TipoUsuario
-SELECT * FROM Especialidade
+
+
--- Scripts/spmedgroup-02-DML.sql
+++ Scripts/spmedgroup-02-DML.sql
@@ -1,38 +1,32 @@
-USE SpMedGroup
+USE SpMedGroup_Tarde;
+GO
 
-SELECT * FROM Clinica
-SELECT * FROM Administrador
 SELECT * FROM Usuario
-SELECT * FROM Medico
 SELECT * FROM Prontuario
 SELECT * FROM Consulta
-SELECT * FROM TipoUsuario
+SELECT * FROM Clinica
+SELECT * FROM Medico
+SELECT * FROM Administrador
 SELECT * FROM Especialidade
+SELECT * FROM Cidade
+SELECT * FROM Estado
+SELECT * FROM TipoUsuario
+SELECT * FROM Situacao
 
-INSERT INTO Clinica (Endereco, Cnpj, HorarioFuncionamento)
-VALUES ('Avenida Alarameda Barão de Limeira, 539', '86400902000130', 'Das 8 ás 18');
+INSERT INTO Estado (NomeEstado)
+VALUES ('São Paulo');
+
+INSERT INTO Cidade (IdEstado, NomeCidade)
+VALUES (1, 'Sao Paulo');
 
 INSERT INTO TipoUsuario (Titulo)
 VALUES ('Administrador'), ('Paciente'), ('Médico');
 
-INSERT INTO Usuario (IdTipoUsuario, Nome, Email, Senha, Telefone, Rg, Cpf, Endereco)
-VALUES (3, 'Ricardo Lemos', 'ricardo.lemos@spmedicalgroup', '123123', '11 1212-1212', '123123321', '11122233300', 'Av. Barão Limeira, 532, São Paulo, SP');
+INSERT INTO Situacao (NomeSituacao)
+VALUES ('Confirmada'), ('Pendente'), ('Cancelada'), ('Realizada');
 
-INSERT INTO Usuario (IdTipoUsuario, Nome, Email, Senha, Telefone, Rg, Cpf, Endereco)
-VALUES (3, 'Roberto Possarle', 'roberto.possarle@spmedicalgroup.com.br', '321321', '11 32323232', '321321321', '33322211100', 'Av. Barão Limeira, 532, São Paulo, SP'), 
-(3, 'Helena Strada', 'helena.souza@spmedicalgroup.com.br', '333333', '11 33333333', '33333333', '33333333333', 'Av. Barão Limeira, 532, São Paulo, SP'),
-(2, 'Ligia', 'ligia@gmail.com', '111111', '11 34567654', '435225435', '94839859000', 'Rua Estado de Israel 240, São Paulo, Estado de São Paulo, 04022-000'),
-(2, 'Alexandre', 'alexandre@gmail.com', '111111', '11 987656543', '326543457', '73556944057', 'Av. Paulista, 1578 - Bela Vista, São Paulo - SP, 01310-200'),
-(2, 'Fernando', 'fernando@gmail.com', '222222', '11 972084453', '546365253', '16839338002', 'Av. Ibirapuera - Indianópolis, 2927,  São Paulo - SP, 04029-200'),
-(2, 'Henrique', 'henrique@gmail.com', '555555', '11 34566543', '543663625', '14332654765', 'R. Vitória, 120 - Vila Sao Jorge, Barueri - SP, 06402-030'),
-(2, 'Joao', 'joao@hotmail.com', '666666', '11 76566377', '325444441', '91305348010', 'R. Ver. Geraldo de Camargo, 66 - Santa Luzia, Ribeirão Pires - SP, 09405-380'),
-(2, 'Bruno', 'bruno@gmail.com', '777777', '11 954368769', '545662667', '79799299004', 'Alameda dos Arapanés, 945 - Indianópolis, São Paulo - SP, 04524-001'),
-(2, 'Mariana', 'mariana@outlook.com', '888888', '' ,'545662668', '13771913039', 'R Sao Antonio, 232 - Vila Universal, Barueri - SP, 06407-140'),
-(1, 'Carol', 'carol@gmail.com', '545454', '11 54545454', '545454556', '12323233421', 'Rua Estado de Israel 17, São Paulo, Estado de São Paulo, 04022-000'),
-(1, 'Saulo', 'saulo@gmail.com', '767676', '11 7676-676', '736432784', '32356236267', 'Rua Estado de Israel 1700, São Paulo, Estado de São Paulo, 04022-000')
-
-INSERT INTO Administrador (IdClinica, IdUsuario)
-VALUES (1, 11),(1, 12);
+INSERT INTO Clinica (IdCidade, NomeClinica, Cnpj, Cep, Logradouro, Bairro, HorarioInicial, HorarioFinal)
+VALUES (1, 'SpMedGroup1', '86400902000130', '01202001', 'Av. Alameda Barão de Limeira, 539', 'Santa Cecilia', '08:00', '18:00');
 
 INSERT INTO Especialidade (Titulo)
 VALUES ('Acupuntura'),
@@ -53,14 +47,37 @@ VALUES ('Acupuntura'),
 ('Pediatria'),
 ('Psiquiatria');
 
+INSERT INTO Usuario (IdTipoUsuario, Nome, Email, Senha, Telefone, Rg, Cpf, Cep, Logradouro, Bairro)
+VALUES (3, 'Ricardo Lemos', 'ricardo.lemos@spmedicalgroup', '123123', '11 1212-1212', '123123321', '11122233300', '01202001','Av. Barão Limeira, 532', 'Santa Cecilia');
+
+INSERT INTO Usuario (IdTipoUsuario, Nome, Email, Senha, Telefone, Rg, Cpf, Cep, Logradouro, Bairro)
+VALUES (3, 'Roberto Possarle', 'roberto.possarle@spmedicalgroup.com.br', '321321', '11 32323232', '321321321', '33322211100', '01202001','Av. Barão Limeira, 532','Santa Cecilia'), 
+(3, 'Helena Strada', 'helena.souza@spmedicalgroup.com.br', '333333', '11 33333333', '33333333', '33333333333', '01202001','Av. Barão Limeira, 532','Santa Cecilia'),
+(2, 'Ligia', 'ligia@gmail.com', '111111', '11 34567654', '435225435', '94839859000', '05678366','Rua Estado de Israel, 240', 'Centro'),
+(2, 'Alexandre', 'alexandre@gmail.com', '111111', '11 987656543', '326543457', '73556944057', '01310200', 'Av. Paulista, 1578', 'Bela Vista'),
+(2, 'Fernando', 'fernando@gmail.com', '222222', '11 972084453', '546365253', '16839338002', '04029200', 'Av. Ibirapuera, 2927', 'Indianópolis'),
+(2, 'Henrique', 'henrique@gmail.com', '555555', '11 34566543', '543663625', '14332654765', '06402030', 'Rua Vitória, 120', 'Vila Jorge'),
+(2, 'Joao', 'joao@hotmail.com', '666666', '11 76566377', '325444441', '91305348010', '09405380','Rua Ver. Geraldo de Camargo, 66', 'Santa Luzia'),
+(2, 'Bruno', 'bruno@gmail.com', '777777', '11 954368769', '545662667', '79799299004', '04524001', 'Av. Alameda dos Arapanés, 945', 'Indianópolis'),
+(2, 'Mariana', 'mariana@outlook.com', '888888', '' ,'545662668', '13771913039', '06407140', 'Rua Sao Antonio, 232', 'Vila Universal'),
+(1, 'Carol', 'carol@gmail.com', '545454', '11 54545454', '545454556', '12323233421', '04022000', 'Rua Estado de Israel, 17', 'Centro'),
+(1, 'Saulo', 'saulo@gmail.com', '767676', '11 7676-676', '736432784', '32356236267', '04022000', 'Rua Estado de Israel 1700', 'Centro');
+
 INSERT INTO Medico (IdEspecialidade, IdClinica, IdUsuario, Crm)
-VALUES (2, 1, 1, '54256-SP'), (16, 1, 2, '54452-SP'), (17, 1, 3, '65463-SP');
+VALUES (2, 1, 1, '54256-SP'), (16, 1, 2,'54452-SP'), (17, 1, 3,'65463-SP');
+
+INSERT INTO Administrador (IdClinica, IdUsuario)
+VALUES (1, 11) , (1, 12);
 
 INSERT INTO Prontuario (IdUsuario, IdClinica, Altura, Peso, DataNascimento)
-VALUES (3, 1, '1,60', '54', '13/10/1983'), (4, 1, '1,80', '75', '23/07/2001'), (5, 1, '1,87', '65', '10/10/1978'), (6, 1, '1,92', '103', '13/10/1985'), (7, 1, '1,43', '58', '27/08/1975'), (8, 1, '1,50', '50', '21/03/1972'), (9, 1, '1,22', '39', '05/03/2018');
+VALUES (4, 1, '1,60', '54', '13/10/1983'), (5, 1, '1,80', '75', '23/07/2001'),
+(6, 1, '1,87', '65', '10/10/1978'), (7, 1, '1,92', '103', '13/10/1985'), 
+(8, 1, '1,43', '58', '27/08/1975'), (9, 1, '1,50', '50', '21/03/1972'),
+(10, 1, '1,22', '39', '05/03/2018');
 
-INSERT INTO Consulta (IdProntuario, IdMedico, DataConsulta, Descricao, Situacao)
-VALUES (7, 3, '20/01/2020 15:00', 'Ressonancia', 'Realizada')
+INSERT INTO Consulta (IdProntuario, IdMedico, IdSituacao, DataConsulta, Descricao)
+VALUES (6, 1, 4, '20/01/2020 15:00', 'Ressonancia'),(1, 2, 3, '06/01/2020 10:00', 'Exame de Rotina'),
+(2, 2, 4, '07/02/2020 11:00', 'Ressonancia'),(5, 2, 4, '06/02/2018 10:00', 'Ressonancia'), 
+(3, 1, 3, '07/02/2019 11:00', 'Ressonancia'),(4, 3, 1, '08/03/2020 15:00', 'Ressonancia'),
+(7, 1, 1, '09/03/2020', 'Exame de Rotina');
 
-INSERT INTO Consulta (IdProntuario, IdMedico, DataConsulta, Descricao, Situacao)
-VALUES (2, 4, '06/01/2020 10:00', 'Exame de Rotina', 'Cancelada'), (3, 4, '07/02/2020 11:00', 'Ressonancia', 'Realizada'), (6, 4, '06/02/2018 10:00', 'Ressonancia', 'Realizada'), (4, 3, '07/02/2019 11:00', 'Ressonancia', 'Cancelada'), (5, 5, '08/03/2020 15:00', 'Ressonancia', 'Agendada') , (8, 3, '09/03/2020', 'Exame de Rotina', 'Agendada');
